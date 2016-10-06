fd84147fff2ab0da9cdaa0e44ec6ae2db835199c
shimamura <14ec063@ms.dendai.ac.jp>
2016-10-06 11:55:22 +0900

    gurusutaHW

--- 7-0.php
+++ 7-0.php
@@ -1,35 +1,45 @@
-<h3>タイムゾーン設定</h3>
+<!--<h3>タイムゾーン設定</h3>
 <?php
 $tz = "Asia/Tokyo";
 if (date_default_timezone_set($tz))
     echo $tz, " OK!";
 ?>
-
-<h3>現在日時</h3>
+-->
 <?php
 $d = date("c"); // ISO8601
-$day = date("Y-m-d"); // 年月日
-$time = date("h:i:s"); // 時分秒
+$day = date("Y年m月d日"); // 年月日
+$time = date("h時i分s秒"); // 時分秒
 $ampm = date("a"); // am/pm
-$week = date("D (w)"); // 曜日
-echo $d, "<br>";
-echo $day, "<br>";
-echo $time, " ", $ampm, "<br>";
-echo $week;
-?>
+$week1 = array("日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日");
+$date = date_create();
+$w = (int)date_format($date, 'w');
+$week = $week1[$w];
+//echo "<table border cellspacing=\"0\" cellpadding=\"3\">
+//<tr>
+//<td bgcolor=\"#778899\">1-1</td>
+//<td>1-2</td>
+//</tr>
+//<tr>
+//<td bgcolor=\"#778899\">2-1</td>
+//<td>2-2</td>
+//</tr>
+//<tr>
+//<td bgcolor=\"#778899\">3-1</td>
+//<td>3-2</td>
+//</tr>
+//</table>";
+echo "<table border cellspacing=\"0\" cellpadding=\"3\">
+<tr>
+<td bgcolor=\"#778899\">日付</td><td>";
+echo $day,"</td>
+</tr>
+<tr>
+<td bgcolor=\"#778899\">時刻</td><td>";
+echo $time, "</td>
+</tr>
+<tr>
+<td bgcolor=\"#778899\">曜日</td><td>";echo $week, "</td>
+</tr>
+</table>";
 
-<h3>エポックタイム</h3>
-<?php
-$now = time();
-$epoc = strtotime("1970-01-01 00:00:00 +09:00");
-echo $now, "<br>";
-echo $epoc, "<br>";
-?>
-
-<h3>strtotime/checkdate関数</h3>
-<?php
-$after_a_month = strtotime("+1 month"); // 本日の1か月後
-echo date("Y-m-d",$after_a_month), "<br>";
-var_dump(checkdate(2, 29, 2016)); echo "<br>";
-var_dump(checkdate(2, 29, 2017)); echo "<br>";
 ?>
